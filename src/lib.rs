533a9039f7a98ab0b00db2929211e6eda14962ce
rkr35 <rkr35@dragons.drexel.edu>
2020-12-31 17:42:41 -0500

    Initialize console

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,20 @@
 use winapi::{
     shared::minwindef::{BOOL, DWORD, HINSTANCE, LPVOID, TRUE},
-    um::libloaderapi,
-    um::processthreadsapi,
     um::winnt::DLL_PROCESS_ATTACH,
+    um::{consoleapi, libloaderapi, processthreadsapi, wincon},
 };
 
 unsafe extern "system" fn my_thread(dll: LPVOID) -> DWORD {
+    consoleapi::AllocConsole();
+
+    println!("A brief moment in time.");
+
+    use std::io::Read;
+    let mut buffer = [0; 1];
+    let _ = std::io::stdin().read_exact(&mut buffer);
+
+    wincon::FreeConsole();
+    libloaderapi::FreeLibraryAndExitThread(dll.cast(), 0);
     0
 }
 
@@ -15,12 +24,12 @@ unsafe extern "system" fn DllMain(dll: HINSTANCE, reason: DWORD, _: LPVOID) -> B
         libloaderapi::DisableThreadLibraryCalls(dll);
 
         processthreadsapi::CreateThread(
-            std::ptr::null_mut(),   // uninheritable, default security descriptor
-            0,                      // use default stack size
-            Some(my_thread),        // function for thread to execute
-            dll.cast(),             // pass handle to dll for thread to deallocate
-            0,                      // run function immediately
-            std::ptr::null_mut(),   // don't return created thread id
+            std::ptr::null_mut(), // uninheritable, default security descriptor
+            0,                    // use default stack size
+            Some(my_thread),      // function for thread to execute
+            dll.cast(),           // pass handle to dll for thread to deallocate
+            0,                    // run function immediately
+            std::ptr::null_mut(), // don't return created thread id
         );
     }
 
