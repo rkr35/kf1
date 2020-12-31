437455ad2cd2ddd82093f347bdc60d6aefa9f1cf
rkr35 <rkr35@dragons.drexel.edu>
2020-12-31 17:31:43 -0500

    Add DllMain

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,28 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+use winapi::{
+    shared::minwindef::{BOOL, DWORD, HINSTANCE, LPVOID, TRUE},
+    um::libloaderapi,
+    um::processthreadsapi,
+    um::winnt::DLL_PROCESS_ATTACH,
+};
+
+unsafe extern "system" fn my_thread(dll: LPVOID) -> DWORD {
+    0
+}
+
+#[no_mangle]
+unsafe extern "system" fn DllMain(dll: HINSTANCE, reason: DWORD, _: LPVOID) -> BOOL {
+    if reason == DLL_PROCESS_ATTACH {
+        libloaderapi::DisableThreadLibraryCalls(dll);
+
+        processthreadsapi::CreateThread(
+            std::ptr::null_mut(),   // uninheritable, default security descriptor
+            0,                      // use default stack size
+            Some(my_thread),        // function for thread to execute
+            dll.cast(),             // pass handle to dll for thread to deallocate
+            0,                      // run function immediately
+            std::ptr::null_mut(),   // don't return created thread id
+        );
     }
+
+    TRUE
 }
